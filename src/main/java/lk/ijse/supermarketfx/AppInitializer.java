ef9ae511544ec79bd740480a6922db8a02f31fec
Ahamed Muiz <ahamedmuiz119@gmail.com>
2025-05-10 23:18:23 +0530

    Initial commit

--- src/main/java/lk/ijse/supermarketfx/AppInitializer.java
+++ src/main/java/lk/ijse/supermarketfx/AppInitializer.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
     @Override
+    
     public void start(Stage primaryStage) throws Exception {
         Parent parent = FXMLLoader.load(getClass().getResource("/view/Dashboard.fxml"));
         primaryStage.setScene(new Scene(parent));
